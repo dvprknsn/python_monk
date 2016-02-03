9dc864bc91f52a731ecf67ff07e568856625cc74
David Parkinson <dvprknsn@gmail.com>
2016-02-03 09:55:48 +0000

    add ScaleConverter 'class' script

---
+++ 05_01_converter.py
@@ -0,0 +1,17 @@
+#05_01_converter
+class ScaleConverter:
+    def __init__(self, units_from, units_to, factor):
+        self.units_from = units_from
+        self.units_to = units_to
+        self.factor = factor
+
+    def description(self):
+        return 'Convert ' + self.units_from + ' to ' + self.units_to
+
+    def convert(self, value):
+        return value * self.factor
+
+cl = ScaleConverter('inches', 'mm', 25)
+print (cl.description)
+print ('converting 2 inches')
+print (str(cl.convert(2)) + cl.units_to)
